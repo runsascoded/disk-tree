4c94ef8e1f3eec4cc894eaf1112fa937b7197fdd
Ryan Williams <ryan@runsascoded.com>
2025-05-11 12:03:32 -0400

    wip sql

--- www/app/actions.ts
+++ www/app/actions.ts
@@ -1,9 +1,33 @@
 'use server';
 
-// import { db } from './db'
+import Database, { Database as DBT } from "better-sqlite3"
+// import { Database } from 'sqlite3'
+// import { open } from 'sqlite'
 
-export async function getNums() {
-  return [ 1, 2, 3 ]
-  // const users = await db.user.findMany();
-  // return users;
+const DB = '/Users/ryan/.config/disk-tree/disk-tree.db'
+
+let db: DBT | null = null;
+
+function getDb() {
+  if (!db) {
+    // Consider adding error handling for database opening
+    db = new Database(DB, { readonly: true });
+    // Optional: Add a shutdown hook if running for a long time
+    // process.on('exit', () => db?.close());
+  }
+  return db;
+}
+
+// const db = open({
+//   filename: DB,
+//   driver: Database,
+// })
+
+export async function getScans() {
+  const db = getDb()
+  const stmt = db.prepare('SELECT * FROM scan');
+  const res = stmt.all();
+  // const res = await (await db).all('SELECT * FROM scan')
+  return res
+  // return [ 1, 2, 3 ]
 }
--- www/app/page.tsx
+++ www/app/page.tsx
@@ -1,60 +1,12 @@
-import Image from "next/image"
-import { getNums } from "@/app/actions"
+import { getScans } from "@/app/actions"
 
 export default async function Home() {
-  const nums = await getNums()
+  const scans = await getScans()
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <div>Data: {JSON.stringify(nums)}</div>
+        <div>Data: {JSON.stringify(scans)}</div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
