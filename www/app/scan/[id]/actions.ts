125264d766e9ce6b240896091bd08c97f710be1d
Ryan Williams <ryan@runsascoded.com>
2025-05-11 22:23:02 -0400

    wip plot

--- www/app/scan/[id]/actions.ts
+++ www/app/scan/[id]/actions.ts
@@ -1,15 +1,23 @@
 import { db, Scan } from "@/app/db"
+import { asyncBufferFromFile, parquetReadObjects } from "hyparquet"
+import { mapValues } from "@rdub/base"
+
+export type Kind = 'file' | 'dir'
 
 export type Row = {
   path: string
   size: number
-  mtime: string
+  mtime: number
   n_desc: number
   n_children: number
+  kind: Kind
+  parent: string | null
 }
 
 export type ScanDetails = {
   scan: Scan
+  root: Row
+  children: Row[]
   rows: Row[]
 }
 
@@ -17,5 +25,49 @@ export async function getScan(id: number): Promise<ScanDetails | undefined> {
   const stmt = db.prepare<[number], Scan>('SELECT * FROM scan WHERE id = ?')
   const scan = stmt.get(id)
   if (!scan) return undefined
+
+  let prefix = scan.path
+  if (!prefix.endsWith('/')) {
+    prefix += '/'
+  }
+
   const { blob } = scan
+  const file = await asyncBufferFromFile(blob)
+  let rows =
+    (await parquetReadObjects({ file }))
+      .map(
+        row => mapValues(
+          row,
+          (_, v) => typeof v === 'bigint' ? parseInt(v) : v
+        )
+      ) as Row[]
+  const levels = 2
+  const [ root, ...rest ] = rows
+  if (root.path != scan.path) {
+    throw new Error(`Root path ${root.path} doesn't match scan path ${scan.path}`)
+  }
+  root.parent = null
+  console.log("root:", root)
+  const children: Row[] = []
+  rows = rest.map(({ path, parent, ...obj }) => {
+    if (!path.startsWith(prefix)) {
+      throw new Error(`Path ${path} doesn't start with ${prefix}`)
+    }
+    path = path.slice(prefix.length)
+    if (parent === root.path) {
+      parent = null
+    } else if (parent?.startsWith(prefix)) {
+      parent = parent.slice(prefix.length)
+    } else {
+      throw new Error(`Path ${path}'s parent ${parent} doesn't start with ${prefix}`)
+    }
+    return { path, parent, ...obj }
+  }).filter(({ path, ...obj }) =>{
+    const depth = path.split('/').length
+    if (depth === 1) {
+      children.push({ path, ...obj })
+    }
+    return depth <= levels
+  })
+  return { scan, root, children, rows }
 }
--- www/app/scan/[id]/page.tsx
+++ www/app/scan/[id]/page.tsx
@@ -1,12 +1,79 @@
-import { getScan } from "@/app//scan//[id]/actions"
+import { getScan } from "@/app/scan/[id]/actions"
+import { Time } from "@/app/time"
+import { Size, sizeStr } from "@/app/size"
+import { FaFileAlt, FaFolder } from "react-icons/fa"
+import { Plot } from "@/components/plot";
+import { basename } from "path";
 
 export default async function Home({ params }: any) {
   let { id } = await params
   id = parseInt(id)
-  const scan = await getScan(id)
-  console.log("scan:", scan)
+  const res = await getScan(id)
+  console.log("scan:", res)
+  if (!res) {
+    return <div>Scan not found</div>
+  }
+
+  const { scan, root, children, rows } = res
+  const now = new Date()
+  // const parents = new Set(rows.map(({ parent }) => parent))
+  const data = [ root, ...rows ]
   return <div>
-    <div>Scan {id}</div>
-    <div>Path: {scan?.path}</div>
+    <h1>{scan.path}</h1>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Path</th>
+            <th>Size</th>
+            <th>Modified</th>
+            <th># Desc.</th>
+            <th># Children</th>
+          </tr>
+        </thead>
+        <tbody>
+          {[ root, ...children ].map(({  path, size, mtime, n_desc, n_children, kind, }, idx) => (
+            <tr key={path} className={idx === 0 ? "root" : ""}>
+              <td>{kind === 'file' ? <FaFileAlt /> : <FaFolder />}</td>
+              <td><code>{path}</code></td>
+              <td><Size size={size} /></td>
+              <td><Time time={mtime * 1000} now={now} /></td>
+              <td>{n_desc}</td>
+              <td>{n_children}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Plot
+        className={"treemap"}
+        // clickHandler={() => {
+        //   console.log("clicked", arguments)
+        // }}
+        data={[{
+          type: 'treemap',
+          name: '',
+          branchvalues: 'total',
+          ids: data.map(({ path }) => path),
+          labels: data.map(({ path }) => basename(path)),
+          parents: data.map(({ path, parent }) => path === root.path ? '' : (parent ?? root.path)),
+          values: data.map(({ path, size }) => size),
+          text: data.map(({ size }) => sizeStr(size)),
+          texttemplate: `%{label}<br>%{text}`,
+          customdata: data.map(({ size }) => size),
+          hovertemplate: `%{label}<br>%{customdata} bytes<br>%{text}`,
+        }]}
+        layout={{
+          paper_bgcolor: 'transparent',
+          margin: { t: 0, r: 0, b: 0, l: 0 },
+          // height: 600,
+        }}
+        config={{
+          autosizable: true,
+          displayModeBar: false,
+          responsive: true,
+        }}
+      />
+    </div>
   </div>
 }
