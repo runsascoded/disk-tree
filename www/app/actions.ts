287186bcfd0776b25d80b54b39458351f7e6748f
Ryan Williams <ryan@runsascoded.com>
2025-05-21 00:39:49 -0400

    www: relpaths/URIs

--- www/app/actions.ts
+++ www/app/actions.ts
@@ -1,9 +1,19 @@
 'use server'
 
 import { Scan } from "./db"
-import { db } from "@/app/db";
+import { db } from "@/app/db"
 
 export async function getScans(): Promise<Scan[]> {
-  const stmt = db.prepare<[], Scan>('SELECT * FROM scan')
+  const stmt = db.prepare<[], Scan>(`
+    SELECT s.*
+    FROM scan s
+    INNER JOIN (
+      SELECT id, MAX(time) as max_time
+      FROM scan
+      GROUP BY path
+    ) latest
+    ON s.id = latest.id
+    ORDER BY s.time DESC
+  `)
   return stmt.all()
 }
