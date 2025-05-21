0c7a1abc6e835cacd242fc7cd8ba74a91d17ca04
Ryan Williams <ryan@runsascoded.com>
2025-05-20 22:42:26 -0400

    wip s3 index

--- src/disk_tree/find/index.py
+++ src/disk_tree/find/index.py
@@ -1,7 +1,11 @@
+from dateutil.parser import parse
 import os
+import re
 from os.path import dirname
-from subprocess import PIPE, Popen
+import subprocess
+from subprocess import PIPE
 from typing import Iterator
+from urllib.parse import urlparse
 
 import pandas as pd
 from tqdm import tqdm
@@ -10,15 +14,73 @@ from utz import o, err
 from disk_tree import time
 
 
+WS = re.compile(r'\s+')
+
+
+def s3_files_iter(path: str) -> Iterator[dict]:
+    cmd = [ 'aws', 's3', 'ls', '--recursive', path ]
+    proc = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE, text=True)
+    root = path
+    url = urlparse(root)
+    bkt = url.netloc
+    key0 = url.path.lstrip('/')
+    dirs = set()
+    with time("s3_files_iter lines"):
+        for line in tqdm(proc.stdout):
+            strs = WS.split(line.rstrip('\n'), 3)
+            mtime_str = f'{strs[0]} {strs[1]}'
+            mtime = int(parse(mtime_str).timestamp())
+            size = int(strs[2])
+            key = strs[3]
+            if not key.startswith(f'{key0}/') and key != key0:
+                raise ValueError(f"{path}: unexpected {key=}")
+            key = key[len(key0)+1:] if key != key0 else ''
+            cur = key
+            new_dirs = []
+            while True:
+                try:
+                    idx = cur.rindex('/')
+                except ValueError:
+                    idx = 0
+                cur = cur[:idx]
+                seen = cur in dirs
+                if not seen:
+                    dirs.add(cur)
+                    new_dirs.append(cur)
+                if not cur or seen:
+                    break
+
+            for d in reversed(new_dirs):
+                yield o(
+                    path=d,
+                    size=0,
+                    mtime=0,
+                    kind='dir',
+                    parent=None if d == key0 else dirname(d),
+                    uri=f's3://{bkt}/{d}',
+                )
+            yield o(
+                path=key,
+                size=size,
+                mtime=mtime,
+                kind='file',
+                parent=None if key == key0 else dirname(key),
+                uri=f's3://{bkt}/{key}',
+            )
+
+
 def files_iter(
     path: str,
     sudo: bool = False,
 ) -> Iterator[dict]:
+    if path.startswith('s3://'):
+        yield from s3_files_iter(path)
+        return
     abspath = os.path.abspath(path)
     cmd = ['gfind', abspath, '-printf', r'%y %s %T@ %p\n']
     if sudo:
         cmd = [ 'sudo', *cmd ]
-    proc = Popen(cmd, stdout=PIPE, stderr=PIPE, text=True)
+    proc = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE, text=True)
     with time("files_iter lines"):
         for line in tqdm(proc.stdout):
             strs = line.rstrip('\n').split(' ', 3)
@@ -44,6 +106,7 @@ def files_iter(
     if code != 0:
         err(f"gfind process exited with return code {code}")
 
+
 def index(
     path: str,
     sudo: bool = False,
@@ -54,6 +117,7 @@ def index(
             dict(**e, n_desc=1, n_children=0)
             for e in files_iter(path0, sudo=sudo)
         ]
+
     df = pd.DataFrame(paths)
     files = df[df.kind == 'file']
     dirs0 = df[df.kind == 'dir']
@@ -62,7 +126,7 @@ def index(
     level = 0
     while True:
         with time(f"index-agg-{level}"):
-            cur = cur[cur.path != path0].copy()
+            cur = cur[cur.path != ''].copy()
             if cur.empty:
                 break
             cur['path'] = cur['parent']
@@ -96,6 +160,7 @@ def index(
             'kind': 'dir',
         }).reset_index(drop=True)
         dirs['parent'] = dirs.path.apply(dirname)
+        dirs['uri'] = dirs.path.apply(lambda p: f'{path0}/{p}' if p else path0)
         return (
             pd.concat(
                 [dirs, files],
--- tests/test_find.py
+++ tests/test_find.py
@@ -1,5 +1,6 @@
 from os.path import join, dirname
-from unittest.mock import patch
+import subprocess
+from unittest.mock import patch, MagicMock
 
 from disk_tree import find
 
@@ -12,13 +13,29 @@ class MockProc:
         pass
 
 
-@patch('utz.proc')
-def test_index(mock_proc):
+@patch('subprocess.Popen')
+def test_index(mock_popen):
     with open(join(TESTDATA, 's8g.txt'), 'r') as f:
         find_txt = f.read()
-    mock_proc.lines.return_value = find_txt
+    mock_proc = MagicMock()
+    mock_proc.stdout = iter(find_txt.splitlines())
+    mock_popen.return_value = mock_proc
     test_path = '/Volumes/s8/gopro'
     df = find.index(test_path)
     df.to_csv(join(TESTDATA, 's8g.csv'), index=False)
     df.to_parquet(join(TESTDATA, 's8g.parquet'), index=False)
     assert len(df) == 875
+
+
+@patch('subprocess.Popen')
+def test_s3_index(mock_popen):
+    with open(join(TESTDATA, 's3.txt'), 'r') as f:
+        find_txt = f.read()
+    mock_proc = MagicMock()
+    mock_proc.stdout = iter(find_txt.splitlines())
+    mock_popen.return_value = mock_proc
+    test_path = 's3://runsascoded/gopro'
+    df = find.index(test_path)
+    df.to_csv(join(TESTDATA, 's3.csv'), index=False)
+    df.to_parquet(join(TESTDATA, 's3.parquet'), index=False)
+    assert len(df) == 952
